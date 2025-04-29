ef44eb61fe07aeb3142245fd42b51d885d515e6c
zaeemzafar10 <zaeem.zafar@salsoft.net>
2025-04-29 16:05:03 +0500

    setup env file with app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QueryModule } from './query/query.module';
-
+import { ConfigModule , ConfigService  } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [QueryModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+       const uri = configService.get<string>('MONGODB_URL');
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
+    QueryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
