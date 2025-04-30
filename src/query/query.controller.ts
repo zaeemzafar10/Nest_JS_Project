83317a311e36a347d4b67993c19e9c1ef1f1776f
zaeemzafar10 <zaeem.zafar@salsoft.net>
2025-04-30 16:41:19 +0500

    for query crud operations

--- src/query/query.controller.ts
+++ src/query/query.controller.ts
@@ -12,14 +12,12 @@ export class QueryController {
     async getAllQueries() {
         return this.queryService.getAllQueries();
     }
-
     @Get(':id')
     async getQueryById(
         @Param('id') id: string
     ) : Promise<Query | null> {
         return this.queryService.getQueryById(id);
     }
-
     @Delete(':id')
     async deleteQuery(
         @Param('id') id: string
@@ -44,8 +42,8 @@ export class QueryController {
 
     @Post()
     @UsePipes(new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
+        whitelist: true, // fields are required but not in payload
+        forbidNonWhitelisted: true, // fields are not in schema and dto
         transform: true,
       }))
     async createQuery(
